eac4c6db294871514def6b534140bb4dd06b065e
John <109565796+jcalhoun-trifinlabs@users.noreply.github.com>
2026-03-05 07:32:48 -0500

    chore: Export AuthGuard from common barrel

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
+export { AuthGuard } from './auth-guard'
 export { ErrorBoundary } from './error-boundary'
 export { LoadingSpinner } from './loading-spinner'
 export { ThemeToggle } from './theme-toggle'
