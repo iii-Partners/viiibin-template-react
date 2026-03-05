ae83f89185b229c7fdd6811535f2cc46c95e1a68
John <109565796+jcalhoun-trifinlabs@users.noreply.github.com>
2026-03-05 07:32:21 -0500

    feat: Add AuthGuard to protected routes (#33)

--- src/app/routes/_auth.tsx
+++ src/app/routes/_auth.tsx
@@ -1,6 +1,10 @@
+import { AuthGuard } from '@/components/common/auth-guard'
 import { AppShell } from '@/components/layout/app-shell'
 
 export default function AuthLayout() {
-  // TODO: Add AuthGuard wrapper here (issue #33)
-  return <AppShell />
+  return (
+    <AuthGuard>
+      <AppShell />
+    </AuthGuard>
+  )
 }
